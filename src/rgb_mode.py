78f951f0b5820c470d95bea4e01513b64a6575bf
Jadon T Schuler <jtschuler007@yahoo.com>
2020-06-25 22:45:07 -0400

    Image sorting works. We may want to change the exact way they're sorted

--- src/rgb_mode.py
+++ src/rgb_mode.py
@@ -1,7 +1,5 @@
-"""Initial testing for getting modal image color"""
+"""Get modal image color"""
 
-import sys
-from PIL import Image
 from scipy import stats
 
 def get_rgb_value(image):
@@ -19,12 +17,4 @@ def get_rgb_value(image):
                                             image.getpixel((i, j))[2])
             pixels.append(html_color)
 
-    print(stats.mode(pixels)[0][0])
     return stats.mode(pixels)[0][0]
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Incorrect number of arguments")
-    else:
-        im = Image.open(sys.argv[1])
-        get_rgb_value(im)
